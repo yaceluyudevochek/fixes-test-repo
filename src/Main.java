89f6e41afd0554769e3c82e6dbd88e3837bad201
yaceluyudevochek <edernem07@gmail.com>
2026-06-02 03:07:07 +0300

    Add Hello message

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+    System.out.println("Hello!");
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     IO.println(String.format("Hello and welcome!"));
